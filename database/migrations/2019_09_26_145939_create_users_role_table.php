37e115be8b2deb9d05f38fcc1489e9c92104f7d9
Андрей Александрович Куренков <AAKurenkov@DVC.RU>
2019-09-27 15:28:47 +0300

    first migrate

--- database/migrations/2019_09_26_145939_create_users_role_table.php
+++ database/migrations/2019_09_26_145939_create_users_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsersRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_role', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-			$table->integer('role');
-            $table->integer('role2');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+			$table->string('name');
+			$table->string('access');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_role');
     }
 }
